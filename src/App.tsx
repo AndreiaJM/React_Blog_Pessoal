8553698da98c5cb2bd319232e3f5391b4385c461
AndreiaJM <andreia.ajms@gmail.com>
2021-12-07 18:23:36 -0300

    criação dos componentes de temas e postagens

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Home from './paginas/home/Home';
 import Login from './paginas/login/Login'
 import './App.css';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+
 
 
 function App() { 
@@ -29,6 +32,14 @@ function App() {
 
           <Route path='/cadastrousuario'>
             <CadastroUsuario/>
+          </Route>
+
+          <Route path='/temas'>
+            <ListaTema/>
+          </Route>
+
+          <Route path='/posts'>
+            <ListaPostagem/>
           </Route>
 
         </div>
